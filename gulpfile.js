1d91d3891dcb021c937bdba76b7870a289d82dbf
Pawel-dev5 <p.nowecki@gmail.com>
2021-08-04 23:03:48 +0200

    Feat: added gulp, gulp-sourcemaps, gulp-sass, sass. Change CSS to SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+// const sass = require("gulp-sass");
+var sass = require('gulp-sass')(require('sass'));
 gulp.task("sass", function() {
     return gulp.src("scss/main.scss")
         .pipe(sass().on("error", sass.logError))
